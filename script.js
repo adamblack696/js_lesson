018c7a7d16e5a3797679ff7c5fd2a2742650ddbd
Adam Black <sinegamy11@gmail.com>
2020-06-05 02:54:52 +0300

    feat: delet

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-let money = 25,
-		income = 100,
-		addExpenses = 25,
-		deposit = 1000,
-		mission = 'Накопить 1 000 000$',
-		period = '1 год';
+let money,
+		income,
+		addExpense,
+		deposit,
+		mission,
+		period;
 
 alert('Учеба началась. Ура!!!');
-console.log('Заработал ' + addExpenses + '$');
-console.log('Потратил ' + income + '$');
-console.log('Цель ' + mission + ' ' + period);
-console.log('Накопленно ' + money + '$');
-	
+console.log('Сообщение в консоли');
