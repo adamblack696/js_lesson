46d5fc70b568af02c0156c26869e8835297983fa
Adam Black <sinegamy11@gmail.com>
2020-06-04 20:59:28 +0300

    add to home work 1

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let money,
-		income,
-		addExpenses,
-		deposit,
-		mission,
-		period;
+let money = 25,
+		income = 100,
+		addExpenses = 25,
+		deposit = 1000,
+		mission = 'Накопить 1 000 000$',
+		period = '1 год';
 
 alert('Учеба началась. Ура!!!');
-
+console.log('Заработал ' + addExpenses + '$');
+console.log('Потратил ' + income + '$');
+console.log('Цель ' + mission + ' ' + period);
+console.log('Накопленно ' + money + '$');
 	
