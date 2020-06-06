45c9c7baf577c3144e15b34bcc34e5405895ffad
Adam Black <sinegamy11@gmail.com>
2020-06-06 09:27:45 +0300

    add new lesson

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let money,
-		income,
-		addExpense,
-		deposit,
-		mission,
-		period;
+let money = 99999,
+		income = 'фриланс',
+		addExpenses = 'такси, еда, квартплата',
+		deposit = true,
+		mission = 9999999,
+		period = 10,
+		budjetDay;
 
-alert('Учеба началась. Ура!!!');
-console.log('Сообщение в консоли');
+console.log('money: ', typeof money);
+console.log('income: ', typeof income);
+console.log('deposit: ', typeof deposit);
+
+console.log('длина addExpense:', addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' долларов');
+
+console.log(addExpenses.toLowerCase().split(','));
+
+budjetDay = money / 30;
+console.log('дневной бюджет = ' + budjetDay);
+
