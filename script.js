34f599a518a562898e6212ca691cfcbecd1e7f2b
Adam Black <sinegamy11@gmail.com>
2020-06-09 18:01:57 +0300

    add lesson03

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 'use strict';
 
-const language = document.querySelector('html').attributes.lang.value,
-dayWeekRu = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
-dayWeekEn = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
-namePerson = 'Артем';
+let money = prompt('Ваш месячный доход?', ''),
+		income = 'фриланс',
+		addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', ''),
+		deposit = confirm('Есть ли у вас депозит в банке?'),
+		mission = 999999,
+		period,
+		expenses1 = prompt('Введите обязательную статью расходов?', ''),
+		expenses2 = prompt('Введите обязательную статью расходов?', ''),
+		amount1 = prompt('Во сколько это обойдется?', ''),
+		amount2 = prompt('Во сколько это обойдется?', ''),
+		budjetMonth,
+		budjetDay;
 
-if(language === 'ru')	console.log(dayWeekRu);
-	else if(language === 'en') console.log(dayWeekEn);
+budjetMonth = money - (+amount1 + +amount2);
 
-switch (language) {
-	case 'ru':
-		console.log(dayWeekRu);
-		break;
-	case 'en':
-		console.log(dayWeekEn);
-		break;
+period = Math.ceil(mission / budjetMonth);
+
+budjetDay = Math.floor(budjetMonth / 30);
+
+console.log('money: ', typeof money);
+console.log('income: ', typeof income);
+console.log('deposit: ', typeof deposit);
+console.log('длина addExpense:', addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' долларов');
+console.log(addExpenses.toLowerCase().split(','));
+console.log('Бюджет на месяц ' + budjetMonth);
+console.log('Цель: ' + mission + '$ будет достигнута через ' + period + ' месяцев');
+console.log('Бюджет на день ' + budjetDay);
+
+if(budjetDay < 0) {
+	console.log('Что то пошло не так');
+} else {
+	if(budjetDay > 1200) console.log('У вас высокий уровень дохода');
+	else if(budjetDay < 600) console.log('К сожалению у вас уровень дохода ниже среднего');
+	else console.log('К сожалению у вас средний уровень дохода');
 }
-
-namePerson === 'Артем' ?  console.log(namePerson + ' директор'):
-namePerson === 'Максим' ? console.log(namePerson + ' преподаватель'):
-console.log(namePerson + 'студент');
